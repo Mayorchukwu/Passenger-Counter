3b07c5566e8b1138bd71c53d5982618c3eedf355
Mayorchukwu <108260673+Mayorchukwu@users.noreply.github.com>
2023-09-13 20:01:53 +0100

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById('count-el')
+console.log(countEl)
+let saveEl = document.getElementById('save-el')
+
+let count = 0
+function increment(){
+    //console.log('Clicked!')
+    count ++
+    countEl.innerText = count
+    console.log(count)
+}
+
+function save(){
+    let countStr = count + ' - '
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    console.log(count)
+}
